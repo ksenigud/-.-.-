0d7787e7d31b6fe73e9f589ef6159cb3a6fe6315
Kseniya Skiba <111737072+ksenigud@users.noreply.github.com>
2022-10-16 23:20:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string[] InputArray() 
+{
+    Console.WriteLine("Enter elements devided by spaces: ");
+    return Console.ReadLine().Split(" ");
+}
+
+int Count(string[] inArray) 
+{
+    int count = 0;
+    for(int i = 0; i < inArray.Length; i++) {
+        if(inArray[i].Length <= 3) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] OutputArray(string[] inArray) 
+{
+    string[] output = new string[Count(inArray)];
+    for(int i = 0, j = 0; i < inArray.Length; i++) 
+    {
+        if(inArray[i].Length <= 3) 
+        {
+            output[j] = inArray[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+string[] inArray = InputArray();
+string[] outArray = OutputArray(inArray);
+Console.WriteLine($"3 or less symbols elements are: {string.Join(", ", outArray)}");
